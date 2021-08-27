8c835385ea133cc40330c36ddfa5cd7a894b99ec
sum529-create <72494319+sum529-create@users.noreply.github.com>
2021-08-27 18:34:49 +0900

    etc update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {HashRouter, BrowserRouter, Route} from "react-router-dom";
-import Navigation from './components/Navigation';
+import React from "react";
+import { HashRouter, BrowserRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
@@ -11,10 +11,10 @@ function App() {
     // BrowserRouter는 HashRouter와 기능은 비슷하지만 '/#/'링크가 없다.
     <BrowserRouter>
       {/* link를 사용하고 있다면 router안에 위치시켜야함 */}
-      <Navigation/>
+      <Navigation />
       {/* 첫번째는 렌더링할 스크린, 두번째는 무엇을 할 지 정해준다. */}
-      <Route path="/about" component={About}/>
-      <Route path="/" exact={true} component={Home}/>
+      <Route path="/movie_app/" exact={true} component={Home} />
+      <Route path="/movie_app/about" component={About} />
       {/*<Route path="/home">
         <h1>Home</h1>
       </Route>
@@ -24,9 +24,9 @@ function App() {
       <Route path="/about">
         <h1>About</h1>
       </Route>*/}
-      <Route path="/movie/:id" component={Detail}/>
-    </BrowserRouter>);
+      <Route path="/movie/:id" component={Detail} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import "./Navigation.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation() {
   return (
     <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-    </div>);
+      <Link to="/movie_app/">Home</Link>
+      <Link to="/movie_app/about">About</Link>
+    </div>
+  );
 }
 
 export default Navigation;
-
